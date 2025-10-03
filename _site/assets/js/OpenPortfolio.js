edd6c419813ddcacbc8353a272feefae0c996ecb
Cassie Parsons <Cassie.Parsons2003@outlook.com>
2025-10-03 15:31:35 +0100

    Done a lot of stuff

---
+++ _site/assets/js/OpenPortfolio.js
@@ -0,0 +1,11 @@
+function ShowDiv(element)
+{
+    console.log(element);
+    document.getElementById(element).style.display = 'block';
+}
+
+function HideDiv(element)
+{
+    console.log(element);
+    document.getElementById(element).style.display = 'none';
+}
